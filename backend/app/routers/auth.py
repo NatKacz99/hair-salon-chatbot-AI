635a591c90c9e968c99bbaa7ab28661ef99bd03a
Natalia Kaczyńska <natalia.kaczynska.programista@gmail.com>
2026-02-21 11:21:15 +0100

    add admins authorization without .gitignore

---
+++ backend/app/routers/auth.py
@@ -0,0 +1,21 @@
+from fastapi import API, Router, Depends, HTTPException
+from sqlalchemy.orm import Session
+from addp.database import get_db
+from app import models
+from app.security import verify_password, create_access_token
+
+router = APIRouter()
+
+@router.post("/login")
+def login(email: str, password: str, db: Session = Depends(get_db)):
+    user = db.query(models.AdminUser).filter(models.AdminUser.email == email).first()
+
+    if not user:
+        raise HTTPException(status_code=401, detail="Wrong email")
+
+    if not verify_password(password, user.hashed_password):
+        raise HTTPException(status_code=401, detail="Wrong password")
+
+    token = create_access_token({"sub": user.email})
+
+    return {"access_token": token}
---
+++ backend/app/security.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+from jose import jwt
+from passlib.context import CryptContext
+import os
+from dotenv import load_dotenv
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+
+password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str):
+    return password_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str):
+    return password_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict):
+    expire = datetime.utcnow() + timedelta(minutes=60)
+    data.update({"exp": expire})
+    return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+
+oauth2scheme = OAUth2PasswordBearer(tokenUrl="login")
+
+def get_current_admin(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithm=[ALGORITHM])
+        email = payload.get("sub")
+        if email is None:
+            raise Exception()
+        return email
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
