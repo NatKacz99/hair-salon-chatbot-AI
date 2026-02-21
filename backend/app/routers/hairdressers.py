3c8d0d02e806dec44b9f68ea1dc4b1b431b140e7
Natalia Kaczyńska <natalia.kaczynska.programista@gmail.com>
2026-02-21 16:31:40 +0100

    add models and endpoints CRUD for hairdressers

--- backend/app/routers/hairdressers.py
+++ backend/app/routers/hairdressers.py
@@ -1,8 +1,30 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app import models
+from app.schemas import HairdresserCreate, HairdresserOut
 from app.security import get_current_admin
+from typing import List
 
 router = APIRouter()
 
-@router.get("/admin-test")
-def admin_test(current_admin: str = Depends(get_current_admin)):
-    return {"message": f"Hello {current_admin}"}
+@router.get("/hairdressers", response_model=List[HairdresserOut])
+def get_hairdressers(db: Session = Depends(get_db)):
+    return db.query(models.Hairdresser).filter(models.Hairdresser.is_active == True).all()
+
+@router.post("/admin/hairdressers", response_model=HairdresserOut)
+def add_hairdresser(data: HairdresserCreate, db: Session = Depends(get_db), admin = Depends(get_current_admin)):
+    hairdresser = models.Hairdresser(**data.dict())
+    db.add(hairdresser)
+    db.commit()
+    db.refresh(hairdresser)
+    return hairdresser
+
+@router.delete("/admin/hairdressers/{id}")
+def delete_hairdresser(id: int, db: Session=Depends(get_db), admin=Depends(get_current_admin)):
+    hairdresser = db.query(models.Hairdresser).filter(models.Hairdresser.id == id).first()
+    if not hairdresser:
+        raise HTTPException(status_code=404, detail="Nie znaleziono fryzjera")
+    hairdresser.is_active = False
+    db.commit()
+    return {"message": "Fryzjer usunięty"}
--- backend/app/security.py
+++ backend/app/security.py
@@ -27,7 +27,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 def get_current_admin(token: str = Depends(oauth2_scheme)):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithm=[ALGORITHM])
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         email = payload.get("sub")
         if email is None:
             raise Exception()
