59fbcfd5d807bb1f97e4106381ca2bdfbfead02c
Natalia Kaczyńska <natalia.kaczynska.programista@gmail.com>
2026-02-24 17:13:33 +0100

    login to admin dashboard

---
+++ frontend/js/admin.js
@@ -0,0 +1,29 @@
+async function login() {
+    const email = document.getElementById('login-email').value;
+    const password = document.getElementById('login-password').value;
+
+    const res = await fetch(`http://127.0.0.1:8000/login?email=${email}&password=${password}`, {
+        method: 'POST'
+    });
+
+    if (res.ok) {
+        const data = await res.json();
+        localStorage.setItem('admin_token', data.access_token);
+        localStorage.setItem('admin_email', email);
+        showDashboardAfterLogin();
+    } else {
+        document.getElementById('login-error').style.display = "block"
+    }
+}
+
+function showDashboardAfterLogin() {
+    document.getElementById('login-page').style.display = 'none';
+    document.getElementById('dashboard').style.display = 'flex';
+}
+
+window.onload = function() {
+    const token = localStorage.getItem('admin_token');
+    if (token) {
+        showDashboardAfterLogin();
+    }
+}
